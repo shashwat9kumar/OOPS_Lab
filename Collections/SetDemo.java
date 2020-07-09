4a1b684e60e067a8424eea99a2740badb8033246
Shashwat Kumar <43718923+shashwat9kumar@users.noreply.github.com>
2020-07-09 20:30:02 +0530

    Create SetDemo.java

---
+++ Collections/SetDemo.java
@@ -0,0 +1,59 @@
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class SetDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		Set<Integer> s = new HashSet<Integer>();
+//		
+//		System.out.println(s.add(32));
+//		System.out.println(s.add(11));
+//		System.out.println(s.add(99));
+//		System.out.println(s.add(99));
+//		System.out.println(s.add(5));
+//		System.out.println(s.add(2));
+//		System.out.println(s.add(8));
+//		
+//		for(int i : s)
+//		{
+//			System.out.println(i);
+//		}
+//		
+//		
+//		Set<Integer> ts = new TreeSet<Integer>();
+//		
+//		System.out.println(ts.add(32));
+//		System.out.println(ts.add(11));
+//		System.out.println(ts.add(99));
+//		System.out.println(ts.add(99));
+//		System.out.println(ts.add(5));
+//		System.out.println(ts.add(2));
+//		System.out.println(ts.add(8));
+//		
+//		
+//		for(int i : ts)
+//		{
+//			System.out.println(i);
+//		}
+//		System.out.println();System.out.println();
+//
+//		String arr = ts.toString();
+//		
+//		
+//		System.out.println(arr.charAt(0));
+//		
+		
+//		HashSet<String> hs = new HashSet<String>();
+//		hs.add("A");
+//		hs.add("B");
+//		hs.add("C");
+//		hs.add("B");
+//		
+//		System.out.println();
+//		System.out.println(hs);
+	}
+
+}
